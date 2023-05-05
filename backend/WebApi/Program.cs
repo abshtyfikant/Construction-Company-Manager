9e9c1673daad43e3517cd9b14dee74ef6c6a56c0
Hieronim <hieronimdaniel@gmail.com>
2023-05-05 21:11:42 +0200

    Connection string and dependency injection

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -5,6 +5,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
