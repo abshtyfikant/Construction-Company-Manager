0121771509ff3bd4bfccea06f13f91718da86460
Hieronim <hieronimdaniel@gmail.com>
2023-05-05 16:50:45 +0200

    Added nu-get packages, dependency injection, automapper and context

---
+++ backend/ConstructionCompanyManager.Infrastructure/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionCompanyManager.Infrastructure
+{
+    public class Context : IdentityDbContext
+    {
+
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
