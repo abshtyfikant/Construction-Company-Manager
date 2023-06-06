ec3c712ae9b628890bf4f97887679947a6ede764
Hieronim <hieronimdaniel@gmail.com>
2023-06-06 14:40:52 +0200

    Added password hashing and removed identity from project

--- backend/Domain/Model/User.cs
+++ backend/Domain/Model/User.cs
@@ -12,6 +12,6 @@ namespace Domain.Model
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
+        public string HashedPassword { get; set; } = null!;
     }
 }
--- backend/Infrastructure/ApplicationDbContext.cs
+++ backend/Infrastructure/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Assigment> Assigments { get; set; }
